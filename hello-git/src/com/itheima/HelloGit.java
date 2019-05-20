de085a5efbda847b8b966516c649985e68b41c23
xiaoafei <xiaoafei@itcast.cn>
2019-05-20 19:13:52 +0800

    添加注释内容

--- hello-git/src/com/itheima/HelloGit.java
+++ hello-git/src/com/itheima/HelloGit.java
@@ -9,10 +9,11 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
-
+        //无聊加点儿注释
     }
         public void show(){
         int i=4;
         int b=i+1;
+            System.out.println("加内容");
         }
 }
